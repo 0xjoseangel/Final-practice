de9fa238700ae43d31465abe5444d50465450ba8
José Ángel Carretero Montes <joseangelcarretero300@gmail.com>
2023-12-15 18:59:32 +0100

    Pintar arreglado!!!!

--- rutas_aereas/src/pruebapintar.cpp
+++ rutas_aereas/src/pruebapintar.cpp
@@ -28,20 +28,22 @@ Imagen Rota(const Imagen & Io,double angulo){
     newimgrows=0;
     newimgcols=0;
     for(int count=0;count<4;count++)
-    {
+	    {
+	    
 	    inter=rcorners[count]*coseno+ccorners[count]*seno;
-	   
 	    if(inter<new_row_min)
 		    new_row_min=inter;
 	    if(inter>new_row_max)
 		    new_row_max=inter;
 	    inter1=-rcorners[count]*seno+ccorners[count]*coseno;
-	   
 	    if(inter1<new_col_min)
 		    new_col_min=inter1;	
 	    if(inter1>new_col_max)
 		    new_col_max=inter1;
-   }
+	    }
+    
+    
+    
     
     newimgrows=(unsigned)ceil((double)new_row_max-new_row_min);
     newimgcols=(unsigned)ceil((double)new_col_max-new_col_min);
@@ -49,32 +51,34 @@ Imagen Rota(const Imagen & Io,double angulo){
     Imagen Iout(newimgrows,newimgcols);
     for(int rows=0;rows<newimgrows;rows++)
       {
-	for(int cols=0;cols<newimgcols;cols++)
-	{
-	   float oldrowcos=((float)rows+new_row_min)*cos(-rads);
-	   float oldrowsin=((float)rows+new_row_min)*sin(-rads);
-	   float oldcolcos=((float)cols+new_col_min)*cos(-rads);
-	   float oldcolsin=((float)cols+new_col_min)*sin(-rads);
-	   float old_row=oldrowcos+oldcolsin;
-	   float old_col=-oldrowsin+oldcolcos;
-	   old_row=ceil((double)old_row);
-	   old_col=ceil((double)old_col);
-	   if((old_row>=0)&&(old_row<Io.num_filas())&&
-	      (old_col>=0)&&(old_col<Io.num_cols()))
-	   {
-	      Iout(rows,cols)=Io(old_row,old_col);
-              
-	   }
-	   else
-	     Iout(rows,cols).r=Iout(rows,cols).g=Iout(rows,cols).b=255;
-	}
+	    for(int cols=0;cols<newimgcols;cols++)
+	    {
+	       float oldrowcos=((float)rows+new_row_min)*cos(-rads);
+	       float oldrowsin=((float)rows+new_row_min)*sin(-rads);
+	       float oldcolcos=((float)cols+new_col_min)*cos(-rads);
+	       float oldcolsin=((float)cols+new_col_min)*sin(-rads);
+	       float old_row=oldrowcos+oldcolsin;
+	       float old_col=-oldrowsin+oldcolcos;
+	       old_row=ceil((double)old_row);
+	       old_col=ceil((double)old_col);
+	       if((old_row>=0)&&(old_row<Io.num_filas())&&
+	          (old_col>=0)&&(old_col<Io.num_cols()))
+	       {
+	          Iout(rows,cols)=Io(old_row,old_col);
+
+	       }
+	       else{
+	         Iout(rows,cols).r=Iout(rows,cols).g=Iout(rows,cols).b=255;
+	         Iout(rows,cols).transp=0;
+	       }
+	    }
     }
     return Iout;
 	  
 }
 
 void Pintar( int f1,int f2,int c1,int c2,Imagen &I, const Imagen &avion,int mindisf,int mindisc){
-      Tipo_Pegado tp = BLENDING;
+      Tipo_Pegado tp = OPACO;
       int fila,col;
       if (abs(f2-f1)>=mindisf || abs(c2-c1)>=mindisc){ 
 	
@@ -129,17 +133,12 @@ int main(int argc, char *argv[]) {
 
     // Supongamos que tienes la imagen del avión cargada previamente
     Imagen avion, mascaraAvion;
-    avion.LeerImagen("./datos/imagenes/aviones/avion2.ppm");
-    mascaraAvion.LeerImagen("./datos/imagenes/aviones/mascara_avion2.pgm");
-    Tipo_Pegado tp = OPACO;
-    Imagen avionRotado;
-    avionRotado.AplicarMascara(mascaraAvion);
-    I.PutImagen(f1,f2,avionRotado,tp);
+    avion.LeerImagen("./datos/imagenes/aviones/avion2.ppm", "./datos/imagenes/aviones/mascara_avion2.pgm");
     int mindisf = 10;
     int mindisc = 10;
 
     // Llamada a la función Pintar
-    // Pintar(f1, f2, c1, c2, I, avion, mindisf, mindisc);
+    Pintar(f1, f2, c1, c2, I, avion, 50, 50);
 
     // Guarda la imagen resultante
     I.EscribirImagen(argv[2]);
